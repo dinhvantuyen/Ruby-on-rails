3b72da60737d85f11ed5ddd91a2148398b491359
dinhvantuyen <dinhtuyen3795@gmail.com>
2018-04-19 11:14:38 +0700

    chapter4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
-# comment 6
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = I18n.t ".title"
+    page_title.empty? ? basetitle : page_title + " | " + base_title
+  end
 end
